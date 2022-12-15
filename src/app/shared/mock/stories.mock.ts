d37409e2e854f840a46e486586b72b99184a6779
Carlos eduardo <carlos.contato00@gmail.com>
2022-12-14 22:07:35 -0300

    Adicionado novas imagens

--- src/app/shared/mock/stories.mock.ts
+++ src/app/shared/mock/stories.mock.ts
@@ -3,110 +3,61 @@ import { StoriesGroup } from '../interfaces/stories-group.interface';
 export const StoriesMock: StoriesGroup[] = [
     {
         id: 0,
-        title: 'Stories 01',
-        background: 'assets/stories/1.jpg',
+        title: 'Família',
+        background: 'assets/stories/familia/capa.jpg',
         date: new Date(),
         items: [
             {
                 id: 0,
                 name: 'Photo 01',
-                url: 'assets/stories/2.jpg',
+                url: 'assets/stories/familia/1.jpg',
             },
             {
                 id: 1,
                 name: 'Photo 02',
-                url: 'assets/stories/4.jpg',
-            },
-            {
-                id: 2,
-                name: 'Photo 03',
-                url: 'assets/stories/5.jpg',
-            },
-            {
-                id: 3,
-                name: 'Photo 04',
-                url: 'assets/stories/2.jpg',
-            },
+                url: 'assets/stories/familia/2.jpg',
+            }
         ],
     },
     {
         id: 1,
-        title: 'Stories 02',
-        background: 'assets/stories/2.jpg',
+        title: 'Trabalho',
+        background: 'assets/stories/trabalho/capa.jpg',
         date: new Date(),
         items: [
             {
                 id: 0,
                 name: 'Photo 01',
-                url: 'assets/stories/5.jpg',
+                url: 'assets/stories/trabalho/1.jpg',
             },
             {
                 id: 1,
                 name: 'Photo 02',
-                url: 'assets/stories/4.jpg',
+                url: 'assets/stories/trabalho/2.jpg',
+            },
+            {
+                id: 2,
+                name: 'Photo 03',
+                url: 'assets/stories/trabalho/3.jpg',
             },
         ],
     },
     {
         id: 2,
-        title: 'Stories 03',
-        background: 'assets/stories/1.jpg',
+        title: 'Viagens',
+        background: 'assets/stories/viagens/1.jpg',
         date: new Date(),
         items: [
             {
                 id: 0,
                 name: 'Photo 01',
-                url: 'assets/stories/2.jpg',
+                url: 'assets/stories/viagens/1.jpg',
             },
             {
                 id: 1,
                 name: 'Photo 02',
-                url: 'assets/stories/4.jpg',
-            },
-            {
-                id: 2,
-                name: 'Photo 03',
-                url: 'assets/stories/5.jpg',
-            },
-            {
-                id: 3,
-                name: 'Photo 04',
-                url: 'assets/stories/2.jpg',
-            },
+                url: 'assets/stories/viagens/2.jpg',
+            }
         ],
     },
-    {
-        id: 3,
-        title: 'Stories 04',
-        background: 'assets/stories/1.jpg',
-        date: new Date(),
-        items: [
-            {
-                id: 0,
-                name: 'Photo 01',
-                url: 'assets/stories/2.jpg',
-            },
-            {
-                id: 1,
-                name: 'Photo 02',
-                url: 'assets/stories/4.jpg',
-            },
-            {
-                id: 2,
-                name: 'Photo 03',
-                url: 'assets/stories/5.jpg',
-            },
-            {
-                id: 3,
-                name: 'Photo 04',
-                url: 'assets/stories/2.jpg',
-            },
-        ],
-    },
-    // {
-    //     title: 'Stories 03',
-    //     background: 'assets/stories/3.jpg',
-    //     date: new Date(),
-    //     stories: [],
-    // }
 ];
