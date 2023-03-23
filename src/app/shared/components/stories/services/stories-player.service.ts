87bfbc64244dfb08552fce5153f810788d3aed22
Carlos eduardo <carlos.contato00@gmail.com>
2023-03-23 17:54:14 -0300

    Adjust

--- src/app/shared/components/stories/services/stories-player.service.ts
+++ src/app/shared/components/stories/services/stories-player.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
+import { StoriesGroup } from 'src/app/shared/interfaces/stories-group.interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class StoriesPlayerService {
+    @Input()
+    public stories: StoriesGroup[];
+
     public constructor() {}
 }
